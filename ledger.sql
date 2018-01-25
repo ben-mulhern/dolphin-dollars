c7234e214949870de557f17fd272b59d26f4dcf6
JHCLLP\haynest <tristan.haynes@jhc.co.uk>
2018-01-25 13:36:25 +0000

    Added constraint to ledger_id.

--- ledger.sql
+++ ledger.sql
@@ -3,6 +3,8 @@ CREATE  TABLE ledger
         PRIMARY KEY (ledger_id),
 
         ledger_id CHAR(10) NOT NULL,
+			CONSTRAINT ledger_id_not_blank
+			CHECK (ledger_id <> '')
 
         description VARCHAR(50) NULL
               
