aa355f2dd674e11873515ee319d35b9b10b88ede
JHCLLP\haynest <tristan.haynes@jhc.co.uk>
2018-01-25 14:09:48 +0000

    Added another constraint for 'description'

--- ledger.sql
+++ ledger.sql
@@ -1,12 +1,14 @@
 CREATE  TABLE ledger
 (
-        PRIMARY KEY (ledger_id),
+  PRIMARY KEY (ledger_id),
 
-        ledger_id CHAR(10) NOT NULL,
-			CONSTRAINT ledger_id_not_blank
-			CHECK (ledger_id <> '')
+    ledger_id CHAR(10) NOT NULL
+      CONSTRAINT ledger_id_not_blank
+      CHECK (ledger_id <> ''),
 
-        description VARCHAR(50) NULL
+    description VARCHAR(50) NOT NULL
+      CONSTRAINT description_not_blank
+      CHECK (description <> '')
               
-)
+);
  
