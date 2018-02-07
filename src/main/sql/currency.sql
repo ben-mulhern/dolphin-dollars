328ab20a4dad3d1fadb8d1ca70d628806be0f34f
Billy Thurley <williamthurley@gmail.com>
2018-02-07 14:15:37 +0000

    currency tweaks, renamed primary key and moved NOT NULL's

--- src/main/sql/currency.sql
+++ src/main/sql/currency.sql
@@ -1,22 +1,23 @@
 CREATE TABLE currency
 (
 
-	PRIMARY KEY PK_currency_code (currency_code),
+    PRIMARY KEY (currency_code),
 	
-	currency_code CHAR(3)
-		NOT NULL,
+    currency_code CHAR(3)
+    NOT NULL,
 	
-	symbol CHAR(1)
-		DEFAULT ' '
-		NOT NULL,
+    symbol CHAR(1)
+    NOT NULL
+    DEFAULT  ' ',
 	
-	description VARCHAR(50)
-		CHECK description <> ' ' 
-		NOT NULL,
+    description VARCHAR(50)
+    NOT NULL
+    CHECK description <> ' ' ,
 	
-	active CHAR(1) 
-		NOT NULL 
-		DEFAULT 'Y' 
-		CHECK active IN ('Y','N')
+    active CHAR(1) 
+    NOT NULL 
+    DEFAULT 'Y' 
+    CHECK active IN ('Y','N')
 
 );
+                                                                                                                                    
