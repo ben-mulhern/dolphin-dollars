210864c540c16515be1c289a048760cef155408c
Ben Mulhern <ben@benmulhern.co.uk>
2018-06-21 12:43:09 +0100

    Needs tidying up, but get user creation service working.

--- src/main/scala/service/user/UserService.scala
+++ src/main/scala/service/user/UserService.scala
@@ -11,7 +11,7 @@ import org.json4s.native.Serialization.read
 import service.ServiceUtilities._
 
 object UserService {
-  class userPassword (user:User,password: String)
+  case class UserPassword (user: User, password: String)
   val UserDal = new UserDal {}
 
   val userService = HttpService {
@@ -19,11 +19,17 @@ object UserService {
       req.decode[String] { data =>
 
         println("here i am")
+        println(data)
         //logger.info("Received request to create new user profile: " + data)
-        val u: User = read[User](data)
-        val p: String = read[String](data)
-        httpJsonResponse(UserDal.createUser(u,p))
+        //val u: User = read[User](data)
+        //val p: String = read[String](data)
+        val up = read[UserPassword](data)
+        println(up)
+        httpJsonResponse(UserDal.createUser(up.user, up.password))
       }
+
+   case req @ GET -> Root / "helloworld" => 
+     httpJsonResponse("Hello world!")
 
 //    case GET -> Root / "player" / userId =>
 //      //logger.info(s"Received request for player $playerId")
