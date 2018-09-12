c3f560dec18bdd46b4409cd34e84a36a92ed7a4a
Billy Thurley <williamthurley@gmail.com>
2018-09-12 13:42:29 +0100

    Heart beat service.

--- src/main/scala/service/heartbeat/package.scala
+++ src/main/scala/service/heartbeat/package.scala
@@ -6,14 +6,11 @@ import org.http4s._
 import org.http4s.dsl._
 import org.json4s.native.Serialization.read
 import service.ServiceUtilities._
-import service.token.TokenService.Jwt
+import service.token.TokenService.getHeartBeatUpdate
 
 package object HeartBeatService {
   val HeartBeatService = HttpService {
-    case req@POST -> Root / "heartbeat" =>
-      req.decode[String] { data =>
-        val JWTToken: Jwt = data
-        httpJsonResponse(JWTToken)
-      }
+    case GET -> Root / "heartBeat" / token =>
+      httpJsonResponse(getHeartBeatUpdate(token))
   }
 }
